8e177b3ce644ee834593e2d50eeb1f0326008096
hyunho4532 <kyounguk192@gmail.com>
2024-10-23 23:02:17 +0900

    Dev . @media 폴더 생성

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,16 +1,18 @@
+import Image from 'next/image'
 import './index.scss'
+import '../../app/styles/@media/index.scss'
 
 export function Header() {
     return (
         <div className="parent">
             <header className="header">
                 <div className="logo">
-                    <p>프리토피아</p>
+                    <Image className='img' src="/logo.png" width={50} height={50} alt={''} />
                 </div>
                 <div className="gnb">
-                    <a href="#">홈</a>
                     <a href="#">로그인</a>
-                    <a href="#">작성</a>
+                    <a href="#">작성함</a>
+                    <a href="#">분석함</a>
                 </div>
                 <div className="profile">
                     <img src="" />
