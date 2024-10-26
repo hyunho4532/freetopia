a8e0714abb6c94b0bd649e1e7ef157c6d3c7b1f1
hyunho4532 <kyounguk192@gmail.com>
2024-10-26 21:15:24 +0900

    Dev . 작성함 기능 구현 중 (입력 필드 데이터 관리 구현 완료)

--- src/components/dialog/write.tsx
+++ src/components/dialog/write.tsx
@@ -8,12 +8,27 @@ import { useDialogState } from "@/features/store/state"
 import { Dialog } from "@mui/material"
 import '../../app/styles/@common/dialog/index.scss'
 import '../../app/styles/@media/dialog/index.scss'
+import { changeWrite, write } from "@/features/store/common/write"
+import { z } from "zod"
 
 type Props = {
     popup: boolean
 }
 
 export function WriteDialog({ popup }: Props) {
+
+    const data = changeWrite();
+
+    const handleChange = (event: any) => {
+
+        const { name, value } = event.target;
+
+        data.setWriteData({
+            ...data.writeData,
+            [name]: value
+        });
+    }
+
     return (
         <Dialog
             PaperProps={{
@@ -33,31 +48,47 @@ export function WriteDialog({ popup }: Props) {
 
             <div className="inputform">
                 <p>1. 현재 직업이 어떻게 되신가요...? ＊</p>
-                <select>
-                    <option>아르바이트</option>
-                    <option>직장인</option>
-                    <option>취준생</option>
+                <select name="job" onChange={handleChange}>
+                    <option value="아르바이트">아르바이트</option>
+                    <option value="학생">직장인</option>
+                    <option value="취준생">취준생</option>
+                    <option value="학생">학생</option>
+                    <option value="기타">기타</option>
                 </select>
 
                 <p>2. 미래를 위해 현재 하고 있는 일이 있나요..? ＊</p>
-                <input type="text" placeholder="ex) 하루동안 꾸준히 운동하기" />
+                <input 
+                    name="work" 
+                    type="text"
+                    placeholder="ex) 하루동안 꾸준히 운동하기"
+                    onChange={handleChange} />
 
                 <p>3. 특정 자격증을 보유하고 계신가요..?</p>
-                <input type="text" placeholder="ex) 한식조리기능사" />
+                <input
+                    name="license"
+                    type="text" 
+                    placeholder="ex) 한식조리기능사"
+                    onChange={handleChange} />
 
                 <p>4. 선호하는 일자리 유형은 어떻게 되시나요..? ＊</p>
-                <select>
-                    <option>단기</option>
-                    <option>장기</option>
-                    <option>주말 알바</option>
-                    <option>재택</option>
+                <select name="workType" onChange={handleChange}>
+                    <option value="단기">단기</option>
+                    <option value="장기">장기</option>
+                    <option value="주말 알바">주말 알바</option>
+                    <option value="재택">재택</option>
                 </select>
 
                 <p>5. 이전에 일했던 경험 및 경력 사항이 있으신가요..?</p>
-                <input type="text" placeholder="간단하게 작성해주세요!" />
+                <input
+                    name="history"
+                    type="text" 
+                    placeholder="간단하게 작성해주세요!"
+                    onChange={handleChange} />
             </div>
 
-            <button className="writeBtn">
+            <button className="writeBtn" onClick={() => {
+                write.parse(data.writeData);
+            }}>
                 현재의 일상을 개선하기 위해 등록하기
             </button>
         </Dialog>
--- src/features/store/common/write.ts
+++ src/features/store/common/write.ts
@@ -1,4 +1,22 @@
+import { useState } from "react";
 import { z } from "zod";
+
+/** 작성함 데이터 
+ * (job: 현재 직업, work: 현재 하고 있는 일, license: 보유하고 있는 자격증, workType: 선호하는 일자리 유형, history: 일한 경험 및 경력 사항) 
+ **/
+export const changeWrite = () => {
+    const [writeData, setWriteData] = useState({
+        job: '',
+        work: '',
+        license: '',
+        workType: '',
+        history: ''
+    })
+
+    return ({
+        writeData, setWriteData
+    });
+}
 
 /** 작성함과 관련된 유효성 검사 데이터  */
 export const write = z.object({
