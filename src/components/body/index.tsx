be82a656d383e3a09d5f1b069b9c676b3b7b38e4
hyunho4532 <kyounguk192@gmail.com>
2024-10-27 10:46:33 +0900

    Dev . 스와이퍼 기능 구현 중

--- src/components/body/index.tsx
+++ src/components/body/index.tsx
@@ -5,6 +5,9 @@ import { useEffect, useState } from 'react'
 import { WriteItems } from '../items/writeItem';
 import tipData from '../../../schema/data.json';
 import { TipItems } from '../items/tipItem';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import swiperData from '../../../schema/data.json';
+import 'swiper/css';
 
 export function Body() {
 
@@ -20,6 +23,18 @@ export function Body() {
     return (
         <div className="body">
             <div className="content">
+                <div className="pagination">
+                    <Swiper
+                        spaceBetween={50}
+                        slidesPerView={3}>
+                        <div className="swiper">
+                            { swiperData.swiper.map((data: any) => (
+                                <SwiperSlide className="slide">{data.title}</SwiperSlide>
+                            ))}
+                        </div>
+                    </Swiper>
+                </div>
+
                 <div className="first">
                     <p>관련 정보 수집 - 분석</p>
                     <TipItems tipData={tipData} />
--- src/components/items/tipItem.tsx
+++ src/components/items/tipItem.tsx
@@ -10,8 +10,9 @@ export function TipItems({ tipData }: Props) {
         <div className='items'>
             { tipData.tip.map((data: any) => (
                 <div className='card'>
-                    <p className='title'>{data.title}</p>
-                    <a href={data.url} target='_blank'>링크 들어가기</a>
+                    <p className='title'>{data.title.substring(0, 14)}...</p>
+                    <p className='link' onClick={() => window.open(data.url, '_blank')}>링크 들어가기 👉</p>
+                    <img className='img' src={data.image} />
                 </div>
             ))}
     </div>
