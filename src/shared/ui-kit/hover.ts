7052a276c0f57adfa5f053001c398f0f404af5fe
hyunho4532 <kyounguk192@gmail.com>
2024-10-31 22:17:21 +0900

    Dev . gsap 라이브러리 임포트, hover.ts x 좌표 이동 수정 완료

--- src/shared/ui-kit/hover.ts
+++ src/shared/ui-kit/hover.ts
@@ -4,13 +4,15 @@ import { MutableRefObject } from "react";
 /** 마우스 영역이 컴포넌트 안쪽에 있을 때 */
 export const handleMouseOver = (index: number, refs: MutableRefObject<HTMLDivElement[]>) => {
     gsap.to(refs.current[index], {
-        opacity: 0
+        x: 30,
+        scale: 1.2
     })
 }
 
 /** 마우스 영역이 컴포넌트 바깥에 있을 때 */
 export const handleMouseOut = (index: number, refs: MutableRefObject<HTMLDivElement[]>) => {
     gsap.to(refs.current[index], {
-        opacity: 1
+        x: 0,
+        scale: 1
     })
 }
